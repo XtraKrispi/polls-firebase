69dd9b61a974f30b114f13f2e5c139d6bf1a499b
Mike <XtraKrispi@gmail.com>
2014-04-30 21:41:50 -0400

    Fixing the poll results to only show one decimal place

--- directive/poll-results/poll-results.js
+++ directive/poll-results/poll-results.js
@@ -9,7 +9,7 @@ angular.module('polls').directive('pollResults', function() {
 		link: function(scope, element, attrs, fn) {
             scope.answerPercentage = function(answer){
                 var totalNumber = _.reduce(scope.poll.answers, function(sum, a) { return sum + a.numberOfAnswers; }, 0);
-                return (answer.numberOfAnswers / totalNumber * 100) || 0;
+                return (answer.numberOfAnswers / totalNumber * 100).toFixed(1) || 0;
             };
 		}
 	};
